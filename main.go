4b40ae8d9804f0974506b4e423a16c37f59dcc11
Hendrik Voßkamp <hendrik.vosskamp@gmail.com>
2024-04-29 16:10:15 +0200

    Loop through date range to create a single json with all holidays

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -35,61 +36,29 @@ func run() error {
 	flag.IntVar(&cliConfig.endYear, "end", 2021, "end year")
 	flag.Parse()
 
-	url := fmt.Sprint("https://feiertage-api.de/api/?jahr=", cliConfig.startYear)
-	fmt.Println(url)
-	resp, err := http.Get(url)
+	var holidays []Holiday
+	for year := cliConfig.startYear; year <= cliConfig.endYear; year++ {
+		url := fmt.Sprint("https://feiertage-api.de/api/?jahr=", year)
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode == http.StatusOK {
+			parseJsonBody(resp.Body, &holidays)
+		}
+	}
+
+	json, err := json.MarshalIndent(holidays, "", "  ")
 	if err != nil {
 		return err
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode == http.StatusOK {
-		decoder := json.NewDecoder(resp.Body)
-
-		var data map[string]interface{}
-		err = decoder.Decode(&data)
-		if err != nil {
-			return err
-		}
-		jsonData, err := json.MarshalIndent(data, "", "  ")
-		if err != nil {
-			return err
-		}
-
-		reader := bytes.NewReader(jsonData)
-
-		var lines []string
-		scanner := bufio.NewScanner(reader)
-		for scanner.Scan() {
-			line := strings.Trim(scanner.Text(), " ")
-			line = strings.ReplaceAll(line, "\"", "")
-			if len(line) > 7 && !strings.Contains(line, "NATIONAL") {
-				lines = append(lines, line)
-			}
-		}
-
-		var holidays []Holiday
-		for i, line := range lines {
-			if strings.Contains(line, "{") {
-				var holiday Holiday
-				holiday.Name = strings.Split(strings.ReplaceAll(line, "{", ""), ":")[0]
-				holiday.Date = strings.Trim(strings.ReplaceAll(strings.Split(lines[i+1], ":")[1], ",", ""), " ")
-				holiday.Info = strings.Trim(strings.Split(lines[i+2], ":")[1], " ")
-				if !holidaysContainsDate(holidays, holiday.Date) {
-					holidays = append(holidays, holiday)
-				}
-			}
-		}
-
-		json, err := json.MarshalIndent(holidays, "", "  ")
-		if err != nil {
-			return err
-		}
-
-		fileName := "parsed-holidays.json"
-		os.WriteFile(fileName, []byte(json), 0644)
-		fmt.Print("Done, file saved to ./", fileName, "\n")
-	}
+	fileName := "parsed-holidays.json"
+	os.WriteFile(fileName, []byte(json), 0644)
+	fmt.Print("Done, file saved to ./", fileName, "\n")
 
 	return nil
 }
@@ -101,4 +70,43 @@ func holidaysContainsDate(holidays []Holiday, date string) bool {
 		}
 	}
 	return false
+}
+
+func parseJsonBody(body io.ReadCloser, holidays *[]Holiday) error {
+	decoder := json.NewDecoder(body)
+
+	var data map[string]interface{}
+	err := decoder.Decode(&data)
+	if err != nil {
+		return err
+	}
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	reader := bytes.NewReader(jsonData)
+
+	var lines []string
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " ")
+		line = strings.ReplaceAll(line, "\"", "")
+		if len(line) > 7 && !strings.Contains(line, "NATIONAL") {
+			lines = append(lines, line)
+		}
+	}
+
+	for i, line := range lines {
+		if strings.Contains(line, "{") {
+			var holiday Holiday
+			holiday.Name = strings.Split(strings.ReplaceAll(line, "{", ""), ":")[0]
+			holiday.Date = strings.Trim(strings.ReplaceAll(strings.Split(lines[i+1], ":")[1], ",", ""), " ")
+			holiday.Info = strings.Trim(strings.Split(lines[i+2], ":")[1], " ")
+			if !holidaysContainsDate(*holidays, holiday.Date) {
+				*holidays = append(*holidays, holiday)
+			}
+		}
+	}
+	return nil
 }
